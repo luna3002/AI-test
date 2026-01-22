252316a206ba36e7e859ca6e63d1fa249b01cb8d
luna3002 <luna3002@innodis.co.kr>
2026-01-22 16:26:29 +0900

    시그모이드 ai serch 페이지 추가 및 지도 삭제

--- innodis/assets/js/map.js
+++ innodis/assets/js/map.js
@@ -1,27 +1,2 @@
-window.initMap = function() {
-    if (document.getElementById('map')) {
-        var mapContainer = document.getElementById('map');
-        
-        var map = new google.maps.Map(mapContainer, {
-            center: {lat: 37.4872877, lng: 127.1197824},
-            zoom: 17,
-            styles: [
-                {
-                    featureType: "poi",
-                    elementType: "labels",
-                    stylers: [{visibility: "off"}]
-                }
-            ]
-        });
-        
-        var marker = new google.maps.Marker({
-            position: {lat: 37.4872877, lng: 127.1197824},
-            map: map,
-            icon: {
-                url: './assets/images/map_marker.png',
-                scaledSize: new google.maps.Size(103, 57),
-                anchor: new google.maps.Point(51, 57)
-            }
-        });
-    }
-};
+// 지도 영역 - API 키 필요 시 추가 가능
+// 현재 공백 상태로 유지
